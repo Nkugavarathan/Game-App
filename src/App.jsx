a823e4ec411c155f5954d924b8190a6a38694d95
Nkugavarathan <kugavarathan28@gmail.com>
2025-07-29 19:30:37 +0530

    inital setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
-import "./css/app.css";
-import MovieCard from "./components/MovieCard";
-import Favourite from "./pages/Favourite";
-import Home from "./pages/Home";
-import { Routes, Route } from "react-router-dom";
+import { Button, ButtonGroup } from "@chakra-ui/react"
 
 function App() {
-  //components name start with capital letter
   return (
     <>
-      <main className="main-content">
-        <Routes>
-          <Route path="" element={<Home />}></Route>
-          <Route path="/favourite" element={<Favourite />}></Route>
-        </Routes>
-      </main>
-      <Home />
+      <Button>clickme</Button>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/pages/Favourite.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Favourite() {
-  return <div className="favourite-">Favourite</div>;
-}
-
-export default Favourite;
